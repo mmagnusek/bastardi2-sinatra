d9355bf9b0afa272434bcbaefa78284f1c3b2e21
Jindrik Faborsky <faborsky@gmail.com>
2011-10-10 23:49:54 +0200

    Available soutez

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -45,6 +45,7 @@ $(document).ready(function() {
   $("#spoty_link").attr('href',"#spoty");
   $("#aktuality_link").attr('href',"#aktuality");
   $("#eshop_link").attr('href',"#eshop");
+  $("#soutez_link").attr('href',"#soutez");
   
   $(".flash").fadeOut(3000);
 });
